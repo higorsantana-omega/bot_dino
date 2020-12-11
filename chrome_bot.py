ed65c3e5e1a640750738893a1d0e0a57b27e912b
higorsantana-omega <omegadosgames02@gmail.com>
2020-12-11 14:50:53 -0300

    x fix

--- chrome_bot.py
+++ chrome_bot.py
@@ -3,18 +3,23 @@ import pyautogui
 import time
 
 # enemy color: 172, 172, 172
-x = 220.0
+X = 270.0
 
 def screen():
     screen_print = ig.grab()
     return screen_print
 
 def enemy(screen_print):
-    for x in range(int(x), int(x+80)):
+    for x in range(int(X), int(X+80)):
         for y in range(410, 465):
             enemy_color = screen_print.getpixel((x, y))
             if enemy_color == (172, 172, 172):
                 return True
+
+def jump():
+    global X
+    pyautogui.press('space')
+    X += 0.8
 
 print('Iniciando em 3 segundos')
 time.sleep(3)
@@ -22,7 +27,5 @@ time.sleep(3)
 while True:
     screen_print = screen()
     if enemy(screen_print):
-        global x
-        pyautogui.press('space')
-        x += 0.5
+        jump()
     print(enemy(screen_print))
